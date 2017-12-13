9ffc565364acaf8386a7756bdea3a3fa5226e85b
andre.pukk <andrep615@hotmail.com>
2017-12-13 14:33:28 +0200

    BREAK operaatoriga väljastab ainult esimese sinise elemendi.

--- tsyklid.php
+++ tsyklid.php
@@ -8,8 +8,14 @@
 for($kord = 1; $kord <= 10; $kord++){
     if($kord % 2 ==0){
         $varv = 'red';
+//        continue;
+        break;
     } else {
         $varv = 'blue';
     }
+//  Kuna värvi defineerimine on juba kirjas
+//    if($varv == 'red'){
+//        continue;
+//    }
     echo '<font color="'.$varv.'">'.$kord.'</font><br />';
 }

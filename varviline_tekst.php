ee7580398c1a613c5707974201ddc34a3466e1cc
andre.pukk <andrep615@hotmail.com>
2017-12-13 15:08:17 +0200

    Värviline tekst

---
+++ varviline_tekst.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andre.pukk
+ * Date: 13.12.2017
+ * Time: 14:55
+ */
+for ($rida = 1; $rida <= 5; $rida++){
+    $varv = '#';
+    for($kord = 1; $kord <= 6; $kord++){
+        $juhuarv = rand(0, 19);
+        $varv = $varv.$juhuarv;
+      //  echo $varv.'<br />';
+    }
+    echo '<font color="'.$varv.'">Värviline tekst</font><br />';
+}
+

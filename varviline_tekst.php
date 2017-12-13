f14b7864f1d7ab7c5c4597d1f1db48978995c32e
andre.pukk <andrep615@hotmail.com>
2017-12-13 15:12:44 +0200

    Automaatuuendus värvide vahetamiseks

--- varviline_tekst.php
+++ varviline_tekst.php
@@ -5,13 +5,15 @@
  * Date: 13.12.2017
  * Time: 14:55
  */
+header('Refresh: 1');
 for ($rida = 1; $rida <= 5; $rida++){
     $varv = '#';
     for($kord = 1; $kord <= 6; $kord++){
-        $juhuarv = rand(0, 19);
+        $juhuarv = rand(0, 15);
         $varv = $varv.$juhuarv;
+        $varv = $varv.$juhuhex;
       //  echo $varv.'<br />';
     }
     echo '<font color="'.$varv.'">Värviline tekst</font><br />';
 }
-sss
+

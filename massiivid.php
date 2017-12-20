6a5c2d92519e337c6f5ed43891e07956687de86e
andre.pukk <andrep615@hotmail.com>
2017-12-20 12:16:30 +0200

    Arrays - for / foreach

---
+++ massiivid.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andre.pukk
+ * Date: 20.12.2017
+ * Time: 12:02
+ */
+$numbers = array(1, 5, 2, 4, 2);
+var_dump($numbers);
+echo '<hr />';
+echo '</pre>';
+print_r($numbers);
+echo '</pre>';
+
+echo '<hr />';
+for($i = 0; $i < count($numbers); $i++){
+    echo '<b>'.$numbers[$i].'</b><br />';
+}
+echo '<hr />';
+foreach ($numbers as $number){
+    echo '<i>'.$number.'</i><br />';
+}

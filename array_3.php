1d55b2e9519b092b3f790fbc6101c15b4099e9a8
andre.pukk <andrep615@hotmail.com>
2018-01-08 08:53:59 +0200

    Array

---
+++ array_3.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andre.pukk
+ * Date: 8.01.2018
+ * Time: 8:49
+ */
+$student = array(
+    'first_name' => 'John',
+    'last_name' => 'Doe',
+    'age' => 16,
+    'class' => 10
+);
+echo '<pre>';
+print_r($student);
+echo '</pre>';
+

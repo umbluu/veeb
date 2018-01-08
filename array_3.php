b7b53660057589396fd21ac02940cb2e14c4d230
andre.pukk <andrep615@hotmail.com>
2018-01-08 09:09:17 +0200

    Array with multiple items

--- array_3.php
+++ array_3.php
@@ -20,3 +20,41 @@ echo '<hr />';
 foreach ($student as $key => $value){
     echo $key.' - '.$value.'<br />';
 }
+
+echo '<hr />';
+
+$students = array(
+    array(
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'age' => 16,
+        'class' => 10
+    ),
+    array(
+        'first_name' => 'Jane',
+        'last_name' => 'Doe',
+        'age' => 15,
+        'class' => 9
+    ),
+    array(
+        'first_name' => 'Tim',
+        'last_name' => 'Toe',
+        'age' => 14,
+        'class' => 8
+    )
+);
+
+echo '<pre>';
+print_r($students);
+echo '</pre>';
+
+echo '<hr />';
+
+foreach ($students as $student){
+    foreach ($student as $key => $value){
+        echo $key.' - '.$value.'<br />';
+    }
+    echo '-------------------'.'<br />';
+}
+
+echo '<hr />';

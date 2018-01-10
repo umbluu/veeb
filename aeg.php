5cc0e643479738c7190e3a7f6202e650ba4c453d
andre.pukk <andrep615@hotmail.com>
2018-01-10 12:38:17 +0200

    Ajatöötlus - ülesanne

--- aeg.php
+++ aeg.php
@@ -10,3 +10,15 @@ echo time().'<br />';
 echo mktime(14, 56, 00, 1, 09, 2018).'<br />';
 $tana = date("H:i");
 echo $tana.'<br />';
+/*
+ * Koosta skript, mis väljastab vormi, mille kaudu
+ * kasutajal on võimalik sisestada oma sünnikuupäev
+ * Arvuta nuppu vajutamisega kasutajale antakse
+ * teada, mitu kuud, päeva, tundi ja minutid on
+ * jäänud antud tähtsa sündmuseni.
+ *
+ * Lahendus peab olema funktsionaalne - eraldi osad
+ * realiseerida funtksioonidega,
+ * võimalusel andmete salvestamiseks kasuta sobilik
+ * andmetüüp - näiteks massiivid, sõned jne
+ * */

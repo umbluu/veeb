4034ae4eb97e52f229f7bb36966b6fdd5f05695b
andre.pukk <andrep615@hotmail.com>
2018-01-15 09:02:36 +0200

    Loodud reaalne objekt ja väljastatud test kujul

---
+++ obj/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andre.pukk
+ * Date: 15.01.2018
+ * Time: 8:58
+ */
+require_once 'text.php';
+
+// Loome reaalse objekti tekst class abil
+
+$minuTekst = new text();
+
+// Teostame test vaate antud objektist
+
+echo '<pre>';
+print_r($minuTekst);
+echo '</pre>';

d2fa6d56fa15ea598204d172d3db7026a52729dd
andre.pukk <andrep615@hotmail.com>
2018-01-15 12:27:08 +0200

    Loodud table class ja defineeritud classi muutujad.

---
+++ obj/table.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andre.pukk
+ * Date: 15.01.2018
+ * Time: 12:23
+ */
+
+class table
+{
+    // Klassi muutujad
+    var $tableContent = array();
+    var $headings = array();
+    var $columnTotal;
+}

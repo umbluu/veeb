ebad6c5238d639de648b082d446c918219e22ee2
andre.pukk <andrep615@hotmail.com>
2018-01-15 08:57:09 +0200

    Class

---
+++ obj/text.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andre.pukk
+ * Date: 15.01.2018
+ * Time: 8:51
+ */
+
+class text
+{
+        // klassi omadus(ed)
+        // klassi muutuja(d)
+        var $string = '';
+}

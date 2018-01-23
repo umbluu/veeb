e09aa322899fd314f59b3fe5eacc0c1dfbe5e858
andre.pukk <andrep615@hotmail.com>
2018-01-23 12:50:15 +0200

    Klass on täiendatud konstandite loomisega

--- mall/model/http.php
+++ mall/model/http.php
@@ -17,10 +17,20 @@ class http
     public function __construct()
     {
         $this->init();
+        $this->initConst();
     }
     // loeme vajalikud väärtused sisse
     function init(){
         $this->vars = array_merge($_GET, $_POST);
         $this->server = $_SERVER;
+    }
+    // loome vajalikud konstandid
+    function initConst(){
+        $constNames = array('HTTP_HOST', 'SCRIPT_NAME', 'REMOTE_ADDR');
+        foreach ($constNames as $constName){
+            if(!defined($constName) and isset($this->server[$constName])){
+                define($constName, $this->server[$constName]);
+            }
+        }
     }
 }

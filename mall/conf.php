1628b618a09059dabf98e96df3a1d42159e7a18c
andre.pukk <andrep615@hotmail.com>
2018-01-23 12:37:31 +0200

    Võetud kasutusele http klassi fail ja loodud tema objekt

--- mall/conf.php
+++ mall/conf.php
@@ -11,3 +11,6 @@ define('VIEW_DIR', 'views/');
 define('CONTROL_DIR', 'controllers/');
 // nõuame vajalike failide kasutamist
 require_once MODEL_DIR.'template.php';
+require_once MODEL_DIR.'http.php';
+// loome objektid, mis oleks vaja pidevalt kasutada
+$http = new http();
